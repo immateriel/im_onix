2bb8221a3ac37bfec318ae849ae5b8fa3abaccec
Manuel Farrica <mfarrica@tea-ebook.com>
2016-11-09 16:43:32 +0100

    Add tests

--- test/test_im_onix.rb
+++ test/test_im_onix.rb
@@ -630,4 +630,100 @@ class TestImOnix < Minitest::Test
       assert_equal "09", @product.sales_restriction.type.code
     end
   end
+
+  context "with only from date" do
+    setup do
+      message = ONIX::ONIXMessage.new
+      message.parse('test/fixtures/test_prices_with_only_from_date.xml')
+
+      @product = message.products.last
+    end
+
+    should "have one supply with 2 price application periods" do
+      assert_equal 1, @product.supplies.size
+
+      prices = @product.supplies.first[:prices]
+
+      assert_equal 2, prices.size
+
+      # the first one: 8.99 € (default price) until 2016-07-07
+      assert_equal 899, prices[0][:amount]
+      assert_equal nil, prices[0][:from_date]
+      assert_equal Date.new(2016, 7, 7), prices[0][:until_date]
+
+      # the second one: 4.99 € from 2016-07-08
+      assert_equal 499, prices[1][:amount]
+      assert_equal Date.new(2016, 7, 8), prices[1][:from_date]
+      assert_equal nil, prices[1][:until_date]
+    end
+  end
+
+  context "with only until date" do
+    setup do
+      message = ONIX::ONIXMessage.new
+      message.parse('test/fixtures/test_prices_with_only_until_date.xml')
+
+      @product = message.products.last
+    end
+
+    should "have one supply with 2 price application periods" do
+      assert_equal 1, @product.supplies.size
+
+      prices = @product.supplies.first[:prices]
+
+      assert_equal 2, prices.size
+
+      # the first one: 4.99 € until 2016-07-07
+      assert_equal 499, prices[0][:amount]
+      assert_equal nil, prices[0][:from_date]
+      assert_equal Date.new(2016, 7, 7), prices[0][:until_date]
+
+      # the second one: 8.99 € (default price) from 2016-07-08
+      assert_equal 899, prices[1][:amount]
+      assert_equal Date.new(2016, 7, 8), prices[1][:from_date]
+      assert_equal nil, prices[1][:until_date]
+    end
+  end
+
+  context "with multiple dates and prices" do
+    setup do
+      message = ONIX::ONIXMessage.new
+      message.parse('test/fixtures/test_prices_with_multiple_periods.xml')
+
+      @product = message.products.last
+    end
+
+    should "have one supply with 5 price application periods" do
+      assert_equal 1, @product.supplies.size
+
+      prices = @product.supplies.first[:prices]
+
+      assert_equal 5, prices.size
+
+      # the first one: 8.99 € (default price) until 2016-07-07
+      assert_equal 899, prices[0][:amount]
+      assert_equal nil, prices[0][:from_date]
+      assert_equal Date.new(2016, 7, 7), prices[0][:until_date]
+
+      # the second one: 4.99 € (promo 1) from 2016-07-08 to 2016-07-08 (single day)
+      assert_equal 499, prices[1][:amount]
+      assert_equal Date.new(2016, 7, 8), prices[1][:from_date]
+      assert_equal Date.new(2016, 7, 8), prices[1][:until_date]
+
+      #the third one: 8.99 € (default price) from 2016-07-09 to 2016-07-31
+      assert_equal 899, prices[2][:amount]
+      assert_equal Date.new(2016, 7, 9), prices[2][:from_date]
+      assert_equal Date.new(2016, 7, 31), prices[2][:until_date]
+
+      #the fourth one: 3.99 € (promo 2) from 2016-08-01 to 2016-08-15
+      assert_equal 399, prices[3][:amount]
+      assert_equal Date.new(2016, 8, 1), prices[3][:from_date]
+      assert_equal Date.new(2016, 8, 15), prices[3][:until_date]
+
+      #the fifth one: 8.99 € (default) from 2016-08-16
+      assert_equal 899, prices[4][:amount]
+      assert_equal Date.new(2016, 8, 16), prices[4][:from_date]
+      assert_equal nil, prices[4][:until_date]
+    end
+  end
 end
