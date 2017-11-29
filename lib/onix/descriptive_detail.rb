76135e75c2729d2673c42aa86c2c7a7db0bcf176
julbouln <jboulnois@gmail.com>
2017-11-29 09:09:31 +0100

    hmpf

--- lib/onix/descriptive_detail.rb
+++ lib/onix/descriptive_detail.rb
@@ -490,7 +490,7 @@ module ONIX
     def keywords
       kws=@subjects.select { |s| s.scheme_identifier.human=="Keywords" }.map { |kw| kw.heading_text }.compact
       kws=kws.flat_map { |kw| kw.split(/;|,|\n/) }.map { |kw| kw.strip }
-      kws.delete_if{|k| k==""}
+      kws.reject!{|k| k==""}
       kws
     end
   end
