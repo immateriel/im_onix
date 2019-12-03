8f835b831ce61980b5fefa07e865f51e956580e8
Guillaume Verger <verger.guillaume@gmail.com>
2019-12-03 09:10:31 +0100

    Add TitleStatement to TitleDetail
    https://www.editeur.org/files/ONIX%203/Changes%20for%20ONIX%203.0.1.pdf

--- lib/onix/descriptive_detail.rb
+++ lib/onix/descriptive_detail.rb
@@ -38,6 +38,7 @@ module ONIX
   class TitleDetail < SubsetDSL
     element "TitleType", :subset
     elements "TitleElement", :subset
+    element "TitleStatement", :text
 
     scope :distinctive_title, lambda { human_code_match(:title_type, /DistinctiveTitle/)}
 
@@ -48,6 +49,8 @@ module ONIX
     # :category: High level
     # flatten title string
     def title
+      return title_statement if title_statement
+
       title_element = @title_elements.product_level #select { |te| te.level.human=~/Product/ }
       if title_element.size > 0
         title_element.first.title
