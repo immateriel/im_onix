c69d0ceaa6992f1852cf665913cd85494d04bae2
julbouln <jboulnois@gmail.com>
2021-01-07 13:41:21 +0100

    1.2.0: fix EpubUsageLimit order

--- lib/onix/epub_usage_limit.rb
+++ lib/onix/epub_usage_limit.rb
@@ -1,6 +1,6 @@
 module ONIX
   class EpubUsageLimit < SubsetDSL
-    element "EpubUsageUnit", :subset, :shortcut => :unit, :cardinality => 1
     element "Quantity", :integer, :cardinality => 1
+    element "EpubUsageUnit", :subset, :shortcut => :unit, :cardinality => 1
   end
 end
