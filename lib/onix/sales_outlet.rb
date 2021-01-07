63dc16228e05f01d9a709eaaa0bcdd1e39227074
julbouln <jboulnois@gmail.com>
2021-01-07 14:12:18 +0100

    1.2.0: fix SalesOutlet

--- lib/onix/sales_outlet.rb
+++ lib/onix/sales_outlet.rb
@@ -1,10 +1,14 @@
 module ONIX
   class SalesOutlet < SubsetDSL
-    elements "SalesOutletIdentifier", :subset, :shortcut => :identifier, :cardinality => 0..n
+    elements "SalesOutletIdentifier", :subset, :shortcut => :identifiers, :cardinality => 0..n
     element "SalesOutletName", :text, :shortcut => :name, :cardinality => 0..1
-  end
 
-  def sales_outlet_identifier
-    self.sales_outlet_identifiers.first
+    def sales_outlet_identifier
+      self.sales_outlet_identifiers.first
+    end
+
+    def identifier
+      self.identifiers.first
+    end
   end
 end
