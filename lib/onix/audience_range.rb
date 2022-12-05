f9a5a496d51cf507ab9192f6d3325760daaa7470
Nitasha Kochar <nitasha@bookbub.com>
2022-12-05 15:19:25 -0500

    Update audience range elements

--- lib/onix/audience_range.rb
+++ lib/onix/audience_range.rb
@@ -1,8 +1,8 @@
 module ONIX
   class AudienceRange < SubsetDSL
     element "AudienceRangeQualifier", :subset, :cardinality => 1
-    element "AudienceRangePrecision", :subset, :cardinality => 1
-    element "AudienceRangeValue", :integer, :cardinality => 1
+    element "AudienceRangePrecision", :subset, :cardinality => 0..n
+    element "AudienceRangeValue", :integer, :cardinality => 0..n
 
     # element "AudienceRangePrecision", :subset, :cardinality => 0..1
     # element "AudienceRangeValue", :integer, :cardinality => 0..1
